ba4f7f484675f512b6b9a675a9c9638b5027e45d
Leonard Truong <leonardtruong@gmail.com>
2014-07-17 19:11:59 -0700

    Adding tests for array ops.

---
+++ test/operations/dense_linear_algebra/test_array_op.py
@@ -0,0 +1,64 @@
+__author__ = 'leonardtruong'
+
+import unittest
+import numpy as np
+
+from teller.operations.dense_linear_algebra.array_op import ArrayAdd, ArrayMul, ArraySub, ArrayDiv
+from teller.types.common import Array
+
+
+class TestArrayOps(unittest.TestCase):
+    def _check(self, specialized, python):
+        rand1 = Array('testArr1', np.random.rand(60, 80).astype(np.float32) * 100)
+        rand2 = Array('testArr2', np.random.rand(60, 80).astype(np.float32) * 100)
+        actual = specialized(rand1, rand2)
+        expected = python(rand1, rand2)
+        try:
+            np.testing.assert_array_almost_equal(actual.data, expected.data, decimal=3)
+        except AssertionError as e:
+            self.fail("Outputs not equal: %s" % e.message)
+
+    def test_simple_array_add(self):
+        self._check(ArrayAdd(backend='ocl'), ArrayAdd(backend='python'))
+
+    def test_simple_array_sub(self):
+        self._check(ArraySub(backend='ocl'), ArraySub(backend='python'))
+
+    def test_simple_array_mul(self):
+        self._check(ArrayMul(backend='ocl'), ArrayMul(backend='python'))
+
+    def test_simple_array_div(self):
+        self._check(ArrayDiv(backend='ocl'), ArrayDiv(backend='python'))
+
+
+class TestNativeArrayOps(unittest.TestCase):
+    def _check(self, actual, expected):
+        try:
+            np.testing.assert_array_almost_equal(actual.data, expected.data, decimal=3)
+        except AssertionError as e:
+            self.fail("Outputs not equal: %s" % e.message)
+
+    def setUp(self):
+        self.rand1 = Array('testArr1', np.random.rand(60, 80).astype(np.float32) * 100)
+        self.rand2 = Array('testArr2', np.random.rand(60, 80).astype(np.float32) * 100)
+
+    def test_native_array_add(self):
+        actual = self.rand1 + self.rand2
+        expected = self.rand1.data + self.rand2.data
+        self._check(actual, expected)
+
+    def test_native_array_sub(self):
+        actual = self.rand1 - self.rand2
+        expected = self.rand1.data - self.rand2.data
+        self._check(actual, expected)
+
+    def test_native_array_mul(self):
+        actual = self.rand1 * self.rand2
+        expected = self.rand1.data * self.rand2.data
+        self._check(actual, expected)
+
+    def test_native_array_div(self):
+        actual = self.rand1 / self.rand2
+        expected = self.rand1.data / self.rand2.data
+        self._check(actual, expected)
+
