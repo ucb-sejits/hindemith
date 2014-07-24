f990873436895cecd3559dee9d7cf072e3e53541
Leonard Truong <leonardtruong@gmail.com>
2014-07-24 14:34:50 -0700

    Fixing up optical flow test.

--- test/test_optical_flow.py
+++ test/test_optical_flow.py
@@ -9,7 +9,7 @@ from hindemith.operations.optical_flow.pyr_up import pyr_up
 from hindemith.operations.optical_flow.warp_img2D import warp_img2d
 from hindemith.operations.dense_linear_algebra import Array, square, Float32
 
-from numpy import random, float32, zeros
+from numpy import random, float32, zeros, testing
 
 __author__ = 'leonardtruong'
 
@@ -85,6 +85,39 @@ def hs_solver_jacobi(im1_data, im2_data, u, v, zero, one, lam2, num_iter):
         dv = vbar - Float32('b', .25) * num * Iy
     return du, dv
 
+def py_hs_solver_jacobi(im1_data, im2_data, u, v, zero, one, lam2, num_iter):
+    du = zero * u
+    dv = zero * v
+
+    tex_Ix0 = stencil1(im1_data)
+    tex_Iy0 = stencil1(im1_data)
+    tex_Ix = stencil1(im2_data)
+    tex_Iy = stencil1(im2_data)
+    tex_Ixx = stencil1(tex_Ix)
+    tex_Iyy = stencil1(tex_Iy)
+    tex_Ixy = stencil1(tex_Iy)
+    u = Array('u', v)
+    v = Array('v', v)
+    Ix = warp_img2d(Array('Ix', tex_Ix), u, v).data
+    Iy = warp_img2d(Array('Iy', tex_Iy), u, v).data
+    It = im1_data - warp_img2d(Array('It', im2_data), u, v).data
+    Ixy = warp_img2d(Array('Ixy', tex_Ixy), u, v).data
+    Ix2 = warp_img2d(Array('Ix2', tex_Ixx), u, v).data
+    Iy2 = warp_img2d(Array('Iy2', tex_Iyy), u, v).data
+    Ix2 = Ix * Ix
+    IxIy = Ix * Iy
+    Iy2 = Ix * Ix
+
+    for i in range(num_iter):
+        ubar = stencil2(du)
+        vbar = stencil2(dv)
+        num = Ix * ubar + Iy * vbar + It
+        # den = Ix2 * Iy2 + lam2
+        den = lam2 + Ix2 * Iy2
+        du = ubar - Ix * num / 4.0
+        dv = vbar - Iy * num / 4.0
+    return du, dv
+
 @fuse
 def update_uv(u, v, du, dv, two, w, h):
     u = u + du
@@ -97,6 +130,19 @@ def update_uv(u, v, du, dv, two, w, h):
     #v = median_filter(v)
     new_u = pyr_up(u)
     new_v = pyr_up(v)
+    return new_u, new_v
+
+def py_update_uv(u, v, du, dv, two, w, h):
+    u = u + du
+    v = v + dv
+    # u = u * two
+    # v = v * two
+    u = two * u
+    v = two * v
+    #u = median_filter(u)
+    #v = median_filter(v)
+    new_u = pyr_up(Array('u', u)).data
+    new_v = pyr_up(Array('v', v)).data
     return new_u, new_v
 
 class TestOpticalFlow(unittest.TestCase):
@@ -121,6 +167,8 @@ class TestOpticalFlow(unittest.TestCase):
 
         hm_u = zeros(sizes[0], dtype=u.dtype)
         hm_v = zeros(sizes[0], dtype=v.dtype)
+        py_hm_u = zeros(sizes[0], dtype=u.dtype)
+        py_hm_v = zeros(sizes[0], dtype=v.dtype)
 
         two = 2.0
 
@@ -134,8 +182,13 @@ class TestOpticalFlow(unittest.TestCase):
             hm_du, hm_dv = hs_solver_jacobi(im1_data=pyr1[i], im2_data=pyr2[i],
                                             u=hm_u, v=hm_v, zero=zero, one=one,
                                             lam2=lam2, num_iter=num_iter)
+            py_hm_du, py_hm_dv = py_hs_solver_jacobi(im1_data=pyr1[i], im2_data=pyr2[i],
+                                            u=py_hm_u, v=py_hm_v, zero=zero, one=one,
+                                            lam2=lam2, num_iter=num_iter)
             hm_u, hm_v = update_uv(u=hm_u, v=hm_v, du=hm_du, dv=hm_dv, two=two,
                                    w=sizes[i + 1][0], h=sizes[i+1][1])
+            py_hm_u, py_hm_v = py_update_uv(u=py_hm_u, v=py_hm_v, du=py_hm_du, dv=py_hm_dv, two=two,
+                                   w=sizes[i + 1][0], h=sizes[i+1][1])
 
-        u = hm_u.data
-        v = hm_v.data
+        testing.assert_array_almost_equal(hm_u.data, py_hm_u)
+        testing.assert_array_almost_equal(hm_v.data, py_hm_v)
