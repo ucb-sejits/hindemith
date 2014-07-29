4fb971e29e69357a89e484ba70336c79b98831a5
chick <chick@qrhino.com>
2014-07-29 11:18:15 -0700

    second kernel seems to be basically working

--- hindemith/operations/matrix_powers.py
+++ hindemith/operations/matrix_powers.py
@@ -79,6 +79,7 @@ class MatrixPowersLazy(LazySpecializedFunction):
         call_args = program_config[0]
 
         base_size = call_args.base_shape[0] * call_args.base_shape[1]
+        border = 1
 
         body = StringTemplate("""
             void __kernel matrix_powers_copy_base_layer(__global const $type* input, __global $type* output) {
@@ -91,12 +92,29 @@ class MatrixPowersLazy(LazySpecializedFunction):
                 int x = get_global_id(0);
                 int y = get_global_id(1);
 
-                matrix[(power+1) * $base_size + y * $len_x + x] = 2 * matrix[power * $base_size + y * $len_x + x];
+                matrix[(power+1) * $base_size + y * $len_x + x] =
+                    0.1f * matrix[
+                        power * $base_size + clamp(y-1, $border, $len_y-$border) * $len_x +  clamp(x, $border, $len_x-$border)
+                    ] +
+                    0.1f * matrix[
+                        power * $base_size + clamp(y+1, $border, $len_y-$border) * $len_x +  clamp(x, $border, $len_x-$border)
+                    ] +
+                    0.4f * matrix[
+                        power * $base_size + clamp(y, $border, $len_y-$border) * $len_x +  clamp(x-1, $border, $len_x-$border)
+                    ] +
+                    0.4f * matrix[
+                        power * $base_size + clamp(y, $border, $len_y-$border) * $len_x +  clamp(x+1, $border, $len_x-$border)
+                    ] +
+                    1.0f * matrix[
+                        power * $base_size + clamp(y, $border, $len_y-$border) * $len_x +  clamp(x, $border, $len_x-$border)
+                    ];
             }
         """, {
             'type': SymbolRef('float'),
             'len_x': Constant(call_args.base_shape[1]),
-            'base_size': Constant(base_size)
+            'len_y': Constant(call_args.base_shape[0]),
+            'base_size': Constant(base_size),
+            'border': Constant(border),
         })
 
         # body = StringTemplate("""
