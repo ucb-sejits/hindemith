a8416ba07c982e331445beadf67cb26b4a854f54
Michael L <michael.lin@sfuhs.org>
2014-07-31 13:46:37 -0700

    finished basic python implementation of reductions.py"

--- hindemith/operations/optical_flow/reductions.py
+++ hindemith/operations/optical_flow/reductions.py
@@ -1,6 +1,5 @@
 from ctree.jit import ConcreteSpecializedFunction, LazySpecializedFunction
 from ctree.transformations import PyBasicConversions
-from hindemith.operations.dense_linear_algebra import Array
 import numpy as np
 import ast
 from ctree.frontend import get_ast
@@ -9,60 +8,75 @@ import inspect
 import textwrap
 from ctypes import *
 from ctree.c.nodes import *
-from hindemith.utils import unique_kernel_name
 from ctree.ocl.macros import *
-from ctree.visitors import NodeTransformer
+from hindemith.types.common import Array
 
-class ReduceConcrete(ConcreteSpecializedFunction):
-    pass
-
+# class ReduceConcrete(ConcreteSpecializedFunction):
+#     pass
+#
 class ReduceLazy(LazySpecializedFunction):
-    def args_to_subconfig(self,args):
-        size = args[0].size
-        type = args[0].dtype
-        return (size,type)
-    def transform(self,tree, program_config):
-        size = program_config[0][0]
-        reducefunc = program_config[0][1]
-        converter = PyBasicConversions()
-        type_dict = {
-            np.float32: c_float(),
-            np.int32: c_int()
-        }
-        type = type_dict[type]
-        reducefunc = converter.visit(reducefunc)
-        reducefunc.return_type = type
-        reducefunc.params[0].type = type
-        reducefunc.params[1].type = type
-        tree = [reducefunc]
-        params = [
-            SymbolRef('input',POINTER(type._ctype_)(),_global=True,_const=True),
-            SymbolRef('temp',POINTER(type._ctype_)(),_global=False,_local=True)
-        ]
-        defn = []
-        defn.extend([
-            Assign(SymbolRef('gid',type),get_global_id(0)),
-            Assign(SymbolRef('lid',type),get_local_id(0)),
-            ast.parse("temp[lid] = input[lid]").body[0],
-            For(Assign(SymbolRef()))
-        ])
+    pass
+#     def args_to_subconfig(self,args):
+#         size = args[0].size
+#         type = args[0].dtype
+#         return (size,type)
+#     def transform(self,tree, program_config):
+#         size = program_config[0][0]
+#         reducefunc = program_config[0][1]
+#         converter = PyBasicConversions()
+#         type_dict = {
+#             np.float32: c_float(),
+#             np.int32: c_int()
+#         }
+#         type = type_dict[type]
+#         reducefunc = converter.visit(reducefunc)
+#         reducefunc.return_type = type
+#         reducefunc.params[0].type = type
+#         reducefunc.params[1].type = type
+#         tree = [reducefunc]
+#         params = [
+#             SymbolRef('input',POINTER(type._ctype_)(),_global=True,_const=True),
+#             SymbolRef('temp',POINTER(type._ctype_)(),_global=False,_local=True),
+#             SymbolRef('output',POINTER(type._ctype_)(),_global=True)
+#         ]
+#         defn = []
+#         defn.extend([
+#             Assign(SymbolRef('gid',c_int()),get_global_id(0)),
+#             Assign(SymbolRef('lsize',c_int()),get_local_size(0)),
+#             Assign(SymbolRef('lid',c_int()),get_local_id(0)),
+#             Assign(ArrayRef(SymbolRef('temp'),SymbolRef('lid')),ArrayRef(SymbolRef('input'),SymbolRef('gid'))),
+#             barrier(CLK_LOCAL_MEM_FENCE()),
+#             For(Assign(SymbolRef('i'),c_int()),Lt(SymbolRef('i'),Constant(0)),Assign(SymbolRef('i'),BitShL(SymbolRef('i'),Constant(0))),
+#                 body=[
+#                     If(Lt(SymbolRef('lid'),SymbolRef('i')),
+#
+#                     )
+#                 ]
+#             )
+#         ])
 
 class Reduce(object):
     def __call__(self, array):
         return ReduceLazy(get_ast(self.func), array)
-    def pure_python(self,array):
-        return reduce(self.func, array.data.asarray)
-    def func(self, x, y):
+    @classmethod
+    def pure_python(cls,array):
+        return reduce(cls.func, array.data.flat)
+    def func(x, y):
         raise NotImplementedError()
 
+## Functions
+
 class Sum(Reduce):
-    def func(self, x, y):
+    @staticmethod
+    def func(x, y):
         return x+y
 
 class Max(Reduce):
-    def func(self, x, y):
+    @staticmethod
+    def func(x, y):
         return x if x > y else y
 
 class Min(Reduce):
+    @staticmethod
     def func(x,y):
         return x if x < y else y
