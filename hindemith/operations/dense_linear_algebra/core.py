0cc89459d2e940127e24df58eb90c3f1081012f6
Leonard Truong <leonardtruong@gmail.com>
2014-07-31 14:57:55 -0700

    Support all array-array, scalar-array, array-scalar operations.

--- hindemith/operations/dense_linear_algebra/core.py
+++ hindemith/operations/dense_linear_algebra/core.py
@@ -31,23 +31,54 @@ class DLAConcreteOCL(ConcreteSpecializedFunction):
         self.global_size = global_size
         return self
 
-    def __call__(self, *args):
-        args += (np.empty_like(args[0]), )
-        bufs = []
+    def process_args(self, *args):
+        processed = []
         events = []
         argtypes = ()
+        output = ct.c_int()
+        out_like = None
         for arg in args:
-            buf, evt = cl.buffer_from_ndarray(self.queue, arg, blocking=False)
-            bufs.append(buf)
-            events.append(evt)
-            argtypes += (cl.cl_mem, )
+            if isinstance(arg, np.ndarray):
+                buf, evt = cl.buffer_from_ndarray(self.queue, arg,
+                                                  blocking=False)
+                processed.append(buf)
+                events.append(evt)
+                argtypes += (cl.cl_mem,)
+                output = buf.empty_like_this()
+                out_like = arg
+            else:
+                processed.append(arg)
+                if isinstance(arg, int):
+                    argtypes += (cl.cl_int,)
+                elif isinstance(arg, float):
+                    argtypes += (cl.cl_float,)
+                    if isinstance(output, ct.c_int):
+                        output = ct.c_float()
+                else:
+                    raise NotImplementedError()
+        if isinstance(output, cl.cl_mem):
+            argtypes += (cl.cl_mem,)
+            processed.append(output)
+        else:
+            processed.append(output.byref)
+            if isinstance(output, ct.c_float):
+                argtypes += (cl.cl_float,)
+            else:
+                argtypes += (cl.cl_int,)
         cl.clWaitForEvents(*events)
+        return processed, argtypes, output, out_like
+
+    def __call__(self, *args):
+        processed, argtypes, output, out_like = self.process_args(*args)
         self.kernel.argtypes = argtypes
-        run_evt = self.kernel(*bufs).on(self.queue, self.global_size)
-        out, evt = cl.buffer_to_ndarray(self.queue, bufs[-1], wait_for=run_evt,
-                                        like=args[0])
-        evt.wait()
-        return out
+        run_evt = self.kernel(*processed).on(self.queue, self.global_size)
+        if isinstance(output, cl.cl_mem):
+            out, evt = cl.buffer_to_ndarray(self.queue, output,
+                                            wait_for=run_evt, like=out_like)
+            evt.wait()
+            return out
+        else:
+            return output.value
 
 
 class PointsLoop(ast.AST):
@@ -83,10 +114,11 @@ class DLAOclTransformer(ast.NodeTransformer):
         self.arg_cfg_dict = {}
         self.params = []
         self.project = None
+        self.loop_var = None
 
     def visit_Project(self, node):
         self.project = node
-        node.files = map(self.visit, node.files)
+        node.files = list(map(self.visit, node.files))
         return node
 
     def visit_FunctionDecl(self, node):
@@ -98,18 +130,20 @@ class DLAOclTransformer(ast.NodeTransformer):
             self.arg_cfg_dict[arg.name] = self.arg_cfg[index]
             arg.type = self.arg_cfg[index].type
         self.params = node.params
-        node.defn = map(self.visit, node.defn)
+        node.defn = list(filter(None, map(self.visit, node.defn)))
         return node
 
     def visit_PointsLoop(self, node):
         self.loop_var = node.loop_var
         body = list(map(self.visit, node.body))
-        params = [SymbolRef(x.name, x.type, _global=True)
+        params = [SymbolRef(x.name, x.type,
+                            _global=self.arg_cfg_dict[x.name].is_global)
                   for x in self.params]
         kernel_name = unique_kernel_name()
         kernel = FunctionDecl(None, kernel_name, params, body)
         kernel.set_kernel()
         self.project.files.append(OclFile(kernel_name, [kernel]))
+        self.loop_var = None
 
     def visit_SymbolRef(self, node):
         if node.name == self.loop_var:
@@ -117,10 +151,9 @@ class DLAOclTransformer(ast.NodeTransformer):
         return node
 
 
-
 HMArray = namedtuple("HMArray", ['type', 'ndpointer', 'shape', 'ndim',
-                                 'length'])
-HMScalar = namedtuple("HMScalar", ['type', 'value'])
+                                 'length', 'is_global'])
+HMScalar = namedtuple("HMScalar", ['type', 'value', 'is_global'])
 
 
 class DLALazy(LazySpecializedFunction):
@@ -133,12 +166,12 @@ class DLALazy(LazySpecializedFunction):
             return HMArray(
                 ndpointer(arg.dtype, arg.ndim, arg.shape)(),
                 ndpointer(arg.dtype, arg.ndim, arg.shape), arg.shape, arg.ndim,
-                reduce(lambda x, y: x * y, arg.shape, 1)
+                reduce(lambda x, y: x * y, arg.shape, 1), True
             )
         elif isinstance(arg, int):
-            return HMScalar(ct.c_int(), arg)
+            return HMScalar(ct.c_int(), arg, False)
         elif isinstance(arg, float):
-            return HMScalar(ct.c_float(), arg)
+            return HMScalar(ct.c_float(), arg, False)
         else:
             raise NotImplementedError()
 
@@ -147,16 +180,24 @@ class DLALazy(LazySpecializedFunction):
 
     def transform(self, tree, program_cfg):
         arg_cfg, tune_cfg = program_cfg
-        arg_cfg += (HMArray(arg_cfg[0].type, arg_cfg[0].ndpointer,
-                            arg_cfg[0].shape, arg_cfg[0].ndim, arg_cfg[0].length), )
+        # FIXME: Assumes all scalars are floats
+        output_type = (HMScalar(ct.POINTER(ct.c_float)(), 0, True),)
+        global_size = 1
+        for arg in arg_cfg:
+            if hasattr(arg, 'ndpointer'):
+                output_type = (HMArray(arg.type, arg.ndpointer, arg.shape,
+                                       arg.ndim, arg.length, True),)
+                global_size = arg.length
+                break
+
         tree = PyBasicConversions().visit(tree)
         tree = DLASemanticTransformer().visit(tree)
-        tree = DLAOclTransformer(arg_cfg).visit(tree)
+        tree = DLAOclTransformer(arg_cfg + output_type).visit(tree)
         fn = DLAConcreteOCL()
         kernel = tree.files[-1]
         program = cl.clCreateProgramWithSource(fn.context,
-                                            kernel.codegen()).build()
-        return fn.finalize(program[kernel.body[0].name], arg_cfg[0].length)
+                                               kernel.codegen()).build()
+        return fn.finalize(program[kernel.body[0].name], global_size)
 
 
 class DLAOp(object):
@@ -172,17 +213,69 @@ class ArrayAdd(DLAOp):
         for x in input1.points():
             output[x] = input1[x] + input2[x]
 
+
 class ArrayMul(DLAOp):
     def op(input1, input2, output):
         for x in input1.points():
             output[x] = input1[x] * input2[x]
+
 
 class ArraySub(DLAOp):
     def op(input1, input2, output):
         for x in input1.points():
             output[x] = input1[x] - input2[x]
 
+
 class ArrayDiv(DLAOp):
     def op(input1, input2, output):
         for x in input1.points():
             output[x] = input1[x] / input2[x]
+
+
+class ScalarArrayAdd(DLAOp):
+    def op(input1, input2, output):
+        for x in input2.points():
+            output[x] = input1 + input2[x]
+
+
+class ScalarArrayMul(DLAOp):
+    def op(input1, input2, output):
+        for x in input2.points():
+            output[x] = input1 * input2[x]
+
+
+class ScalarArraySub(DLAOp):
+    def op(input1, input2, output):
+        for x in input2.points():
+            output[x] = input1 - input2[x]
+
+
+class ScalarArrayDiv(DLAOp):
+    def op(input1, input2, output):
+        for x in input2.points():
+            output[x] = input1 / input2[x]
+
+
+class ArrayScalarAdd(DLAOp):
+    def op(input1, input2, output):
+        for x in input2.points():
+            output[x] = input1[x] + input2
+
+
+class ArrayScalarMul(DLAOp):
+    def op(input1, input2, output):
+        for x in input2.points():
+            output[x] = input1[x] * input2
+
+
+class ArrayScalarSub(DLAOp):
+    def op(input1, input2, output):
+        for x in input2.points():
+            output[x] = input1[x] - input2
+
+
+class ArrayScalarDiv(DLAOp):
+    def op(input1, input2, output):
+        for x in input2.points():
+            output[x] = input1[x] / input2
+
