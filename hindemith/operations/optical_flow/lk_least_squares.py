c481e20034c6d96317722425b9a7262a0f32e058
chick <chick@qrhino.com>
2014-08-01 09:20:30 -0700

    more least squares, kinda runs until it blows up

--- hindemith/operations/optical_flow/lk_least_squares.py
+++ hindemith/operations/optical_flow/lk_least_squares.py
@@ -2,6 +2,7 @@
 compute least squares vector (du,dv)
 based on original LKLeastSquares.cpp
 """
+from stencil_code.stencil_grid import StencilGrid
 from stencil_code.stencil_kernel import StencilKernel
 from hindemith.types.stencil import Stencil
 from hindemith.utils import clamp
@@ -9,25 +10,31 @@ import numpy as np
 
 __author__ = 'chick'
 
+class LKLSGrid(StencilGrid):
+    def __init__(self, numpy_array, radius):
+        super(LKLSGrid, self).__init__(numpy_array.shape)
+        self.set_neighborhood(0,
+            [(x, y) for x in range(-radius, radius + 1) for y in range(-radius, radius + 1)]
+        )
 
-class LkLeastSquares(StencilKernel):
+class LkLeastSquares(object):
     def __init__(self, radius):
-        super(LkLeastSquares, self).__init__()
+        # super(LkLeastSquares, self).__init__()
         self.radius = radius
 
     class DuComputer(StencilKernel):
         def __init__(self):
-            super(LkLeastSquares.DuComputer, self).__init__()
+            super(LkLeastSquares.DuComputer, self).__init__(pure_python=True)
 
         def kernel(self, image1, image2, err, du):
             assert len(image1.shape) == 2
 
-            mx = image1.shape[1]
-            my = image1.shape[2]
+            mx = image1.shape[0]
+            my = image1.shape[1]
             for p in image1.interior_points():
                 ix2 = iy2 = ix_iy = ix_it = iy_it = 0.0
 
-                for n_p in image1.neighbor_points():
+                for n_p in image1.neighbors(p, 0):
                     clamp_x = clamp(n_p[0], 0, mx)
                     clamp_y = clamp(n_p[1], 0, my)
                     ix2 += image1[clamp_x, clamp_y] * image1[clamp_x, clamp_y]
@@ -44,13 +51,13 @@ class LkLeastSquares(StencilKernel):
 
     class DvComputer(StencilKernel):
         def __init__(self):
-            super(LkLeastSquares.DvComputer, self).__init__()
+            super(LkLeastSquares.DvComputer, self).__init__(pure_python=True)
 
         def kernel(self, image1, image2, err, dv):
             assert len(image1.shape) == 2
 
-            mx = image1.shape[1]
-            my = image1.shape[2]
+            mx = image1.shape[0]
+            my = image1.shape[1]
             for p in image1.interior_points():
                 ix2 = iy2 = ix_iy = ix_it = iy_it = 0.0
 
@@ -70,27 +77,21 @@ class LkLeastSquares(StencilKernel):
                     dv[p] = 0.0
 
     def kernel(self, image1, image2, err, du, dv):
-        assert len(image1.shape) == 2 and len(image2) == 2
+        assert len(image1.shape) == 2 and len(image2.shape) == 2
 
-        image_1_stencil = Stencil(image1)
-        image_1_stencil.set_neighborhood( 0,
-            [(x, y) for x in range(-self.radius, self.radius + 1) for y in range(-self.radius, self.radius + 1)]
-        )
+        image_1_stencil = LKLSGrid(image1, self.radius)
+        image_2_stencil = LKLSGrid(image2, self.radius)
 
-        image_2_stencil = Stencil(image2)
-        image_2_stencil.set_neighborhood( 0,
-            [(x, y) for x in range(-self.radius, self.radius + 1) for y in range(-self.radius, self.radius + 1)]
-        )
+        LkLeastSquares.DuComputer().kernel(image_1_stencil, image_2_stencil, err, du)
+        LkLeastSquares.DvComputer().kernel(image_1_stencil, image_2_stencil, err, dv)
 
-        LkLeastSquares.ComputeDu()(image_1_stencil, image_2_stencil, err, du)
-        LkLeastSquares.ComputeDv()(image_1_stencil, image_2_stencil, err, dv)
 
 if __name__ == '__main__':
     image1 = np.random.random([10, 10])
     image2 = image1.copy()
-    error = np.random.random([10, 10])
+    error = np.zeros([10, 10], dtype=np.float32)
 
-    print(image2)
+    print "image 2 shape {} len {}".format(image2.shape, len(image2.shape))
     for i in range(3, 6):
         image2[i][2:7] = list(reversed(image2[i][2:7]))
 
@@ -100,6 +101,10 @@ if __name__ == '__main__':
     dv = np.empty_like(image2)
 
     lk_least_squares = LkLeastSquares(1)
+
+    lk_least_squares.kernel(image1, image2, error, du, dv)
+
+    print du
 
 
 
