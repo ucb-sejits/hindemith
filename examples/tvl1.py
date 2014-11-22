4a573faadae229c02bf47af0082b9965d1749903
Leonard Truong <leonardtruong@gmail.com>
2014-11-22 12:50:07 -0800

    WIP tvl1 refactor

--- examples/tvl1.py
+++ examples/tvl1.py
@@ -15,8 +15,8 @@ ZipWith.backend = 'ocl'
 SpecializedMap.backend = 'ocl'
 
 
-num_warps = 1
-n_scales = 1
+num_warps = 5
+n_scales = 5
 n_inner = 30
 n_outer = 10
 median_filtering = 5
@@ -214,67 +214,112 @@ def py_flow(I0, I1, u1, u2):
 
 
 def compute_flow(I0, I1, u1, u2):
-    p11 = hmarray(np.zeros(I1.shape, dtype=np.float32))
-    p12 = hmarray(np.zeros(I1.shape, dtype=np.float32))
-    p21 = hmarray(np.zeros(I1.shape, dtype=np.float32))
-    p22 = hmarray(np.zeros(I1.shape, dtype=np.float32))
+    p11 = np.zeros(I1.shape, dtype=np.float32)
+    p12 = np.zeros(I1.shape, dtype=np.float32)
+    p21 = np.zeros(I1.shape, dtype=np.float32)
+    p22 = np.zeros(I1.shape, dtype=np.float32)
     I1y, I1x = centered_gradient(I1)
     I1x = I1x.astype(np.float32)
     I1y = I1y.astype(np.float32)
-    I0 = hmarray(I0)
-
-    u1, u2 = hmarray(u1), hmarray(u2)
     for w in range(num_warps):
         _f1, _f2 = build_flow_map(u1, u2)
         I1w = warp(I1, _f1, _f2)
         I1wx = warp(I1x, _f1, _f2)
         I1wy = warp(I1y, _f1, _f2)
-        I1w = hmarray(I1w)
-        I1wx = hmarray(I1wx)
-        I1wy = hmarray(I1wy)
-        grad = square(I1wx) + square(I1wy)
+        grad = np.square(I1wx) + np.square(I1wy)
         rho_c = I1w - I1wx * u1 - I1wy * u2 - I0
         n0 = 0
         error = sys.maxint
         while n0 < n_outer and error > epsilon * epsilon * I0.size:
             # u1 = cv2.medianBlur(u1, median_filtering)
             # u2 = cv2.medianBlur(u2, median_filtering)
-            # u1, u2 = hmarray(u1), hmarray(u2)
             n1 = 0
             while n1 < n_inner and error > epsilon * epsilon * I0.size:
-                v1, v2 = threshold(u1, u2, rho_c, grad, I1wx, I1wy)
-                div_p1 = divergence(p11, p12)
-                div_p2 = divergence(p21, p22)
-                # u1_old = hmarray(u1)
-                # u2_old = hmarray(u2)
+                v1, v2 = py_threshold(u1, u2, rho_c, grad, I1wx, I1wy)
+                div_p1 = py_divergence(p11, p12)
+                div_p2 = py_divergence(p21, p22)
                 u1_old = u1
                 u2_old = u2
                 u1 = v1 + div_p1 * theta
                 u2 = v2 + div_p2 * theta
-                err = square(u1 - u1_old) + square(u2 - u2_old)
-                err.copy_to_host_if_dirty()
-                error = np.sum(err)
-                # u1.copy_to_host_if_dirty()
-                # u2.copy_to_host_if_dirty()
-                # u1 = np.copy(u1)
-                # u2 = np.copy(u2)
-                u1x, u1y = forward_gradient(u1)
-                u2x, u2y = forward_gradient(u2)
-                ng1 = 1.0 + tau / theta * sqrt(square(u1x) + square(u1y))
-                ng2 = 1.0 + tau / theta * sqrt(square(u2x) + square(u2y))
-                # hypot1 = sqrt(square(u1x) + square(u1y))
-                # hypot2 = sqrt(square(u2x) + square(u2y))
+                error = np.sum(np.square(u1 - u1_old) + np.square(u2 - u2_old))
+                u1x, u1y = py_forward_gradient(u1)
+                u2x, u2y = py_forward_gradient(u2)
+                ng1 = 1.0 + tau / theta * np.sqrt(np.square(u1x) +
+                                                  np.square(u1y))
+                ng2 = 1.0 + tau / theta * np.sqrt(np.square(u2x) +
+                                                  np.square(u2y))
                 p11 = (p11 + tau / theta * u1x) / ng1
                 p12 = (p12 + tau / theta * u1y) / ng1
                 p21 = (p21 + tau / theta * u2x) / ng2
                 p22 = (p22 + tau / theta * u2y) / ng2
                 n1 += 1
             n0 += 1
-            # u1 = np.copy(u1)
-            # u2 = np.copy(u2)
-            u1.copy_to_host_if_dirty()
-            u2.copy_to_host_if_dirty()
     return u1, u2
+
+
+# def compute_flow(I0, I1, u1, u2):
+#     p11 = hmarray(np.zeros(I1.shape, dtype=np.float32))
+#     p12 = hmarray(np.zeros(I1.shape, dtype=np.float32))
+#     p21 = hmarray(np.zeros(I1.shape, dtype=np.float32))
+#     p22 = hmarray(np.zeros(I1.shape, dtype=np.float32))
+#     I1y, I1x = centered_gradient(I1)
+#     I1x = I1x.astype(np.float32)
+#     I1y = I1y.astype(np.float32)
+#     I0 = hmarray(I0)
+
+#     u1, u2 = hmarray(u1), hmarray(u2)
+#     for w in range(num_warps):
+#         _f1, _f2 = build_flow_map(u1, u2)
+#         I1w = warp(I1, _f1, _f2)
+#         I1wx = warp(I1x, _f1, _f2)
+#         I1wy = warp(I1y, _f1, _f2)
+#         I1w = hmarray(I1w)
+#         I1wx = hmarray(I1wx)
+#         I1wy = hmarray(I1wy)
+#         grad = square(I1wx) + square(I1wy)
+#         rho_c = I1w - I1wx * u1 - I1wy * u2 - I0
+#         n0 = 0
+#         error = sys.maxint
+#         while n0 < n_outer and error > epsilon * epsilon * I0.size:
+#             # u1 = cv2.medianBlur(u1, median_filtering)
+#             # u2 = cv2.medianBlur(u2, median_filtering)
+#             # u1, u2 = hmarray(u1), hmarray(u2)
+#             n1 = 0
+#             while n1 < n_inner and error > epsilon * epsilon * I0.size:
+#                 v1, v2 = threshold(u1, u2, rho_c, grad, I1wx, I1wy)
+#                 div_p1 = divergence(p11, p12)
+#                 div_p2 = divergence(p21, p22)
+#                 # u1_old = hmarray(u1)
+#                 # u2_old = hmarray(u2)
+#                 u1_old = u1
+#                 u2_old = u2
+#                 u1 = v1 + div_p1 * theta
+#                 u2 = v2 + div_p2 * theta
+#                 err = square(u1 - u1_old) + square(u2 - u2_old)
+#                 err.copy_to_host_if_dirty()
+#                 error = np.sum(err)
+#                 # u1.copy_to_host_if_dirty()
+#                 # u2.copy_to_host_if_dirty()
+#                 # u1 = np.copy(u1)
+#                 # u2 = np.copy(u2)
+#                 u1x, u1y = forward_gradient(u1)
+#                 u2x, u2y = forward_gradient(u2)
+#                 ng1 = 1.0 + tau / theta * sqrt(square(u1x) + square(u1y))
+#                 ng2 = 1.0 + tau / theta * sqrt(square(u2x) + square(u2y))
+#                 # hypot1 = sqrt(square(u1x) + square(u1y))
+#                 # hypot2 = sqrt(square(u2x) + square(u2y))
+#                 p11 = (p11 + tau / theta * u1x) / ng1
+#                 p12 = (p12 + tau / theta * u1y) / ng1
+#                 p21 = (p21 + tau / theta * u2x) / ng2
+#                 p22 = (p22 + tau / theta * u2y) / ng2
+#                 n1 += 1
+#             n0 += 1
+#             # u1 = np.copy(u1)
+#             # u2 = np.copy(u2)
+#             u1.copy_to_host_if_dirty()
+#             u2.copy_to_host_if_dirty()
+#     return u1, u2
 
 
 def py_tvl1(im0, im1):
@@ -326,6 +371,7 @@ u1, u2 = tvl1(im0, im1)
 np.testing.assert_array_almost_equal(py_u1, np.copy(u1), decimal=1)
 np.testing.assert_array_almost_equal(py_u2, np.copy(u2), decimal=1)
 print("Passed")
+exit()
 # np.save("u1-cached", u1)
 # np.save("u2-cached", u2)
 # u1_expected = np.load(file_path + "/u1-cached.npy")
