358e67b6edffdd3944a69b433ec4fc91e993e6e8
Leonard Truong <leonardtruong@gmail.com>
2015-01-28 22:55:30 -0800

    prep for 0.1.0

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='hindemith',
-    version='0.0.2a5',
+    version='0.1.0',
     author='Leonard Truong',
     author_email='leonardtruong@berkeley.edu',
     description='Package containing a suite of high performance pattern specializers',
     url='https://github.com/ucb-sejits/hindemith',
 
-    packages=find_packages(exclude=['examples']),
+    packages=find_packages(exclude=['examples', 'tests']),
 
     install_requires=[
         'ctree',
