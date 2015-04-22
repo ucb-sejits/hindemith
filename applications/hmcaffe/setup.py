78ff50c55b45b6b9c598140bd1f95eb3a5b01e37
Leonard Truong <leonardtruong@gmail.com>
2015-04-22 10:04:24 -0700

    Make it a real package

---
+++ applications/hmcaffe/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hmcaffe',
+    version='0.1.0',
+    author='Leonard Truong',
+    author_email='leonardtruong@berkeley.edu',
+    description='',
+    url='https://github.com/ucb-sejits/hindemith',
+
+    install_requires=[
+        'hindemith'
+    ]
+)
