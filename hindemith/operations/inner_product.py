02621f0535b17855c716a0dcc0a9a4f557bf5998
Leonard Truong <leonardtruong@gmail.com>
2015-05-18 10:59:41 -0700

    Fix inner product bugs

--- hindemith/operations/inner_product.py
+++ hindemith/operations/inner_product.py
@@ -14,8 +14,8 @@ class InnerProductForward(DeviceLevel):
     if backend in {"ocl", "opencl", "OCL"}:
         @classmethod
         def get_launcher(cls, sources, sinks, keywords, symbol_table):
-            bottom = symbol_table[sources[0]]
-            top = symbol_table[sinks[0]]
+            bottom = symbol_table[sources[0].name]
+            top = symbol_table[sinks[0].name]
             bias_multiplier = hmarray((1, bottom.shape[0]))
             bias_multiplier.fill(1)
             bias_multiplier.sync_ocl()
@@ -24,17 +24,17 @@ class InnerProductForward(DeviceLevel):
             M = bottom.shape[0]
             class InnerProductLauncher(object):
                 def __init__(self, sources, sinks):
-                    self.sources = [ast.Name(s, ast.Load()) for s in sources]
-                    self.sinks = [ast.Name(s, ast.Load()) for s in sinks]
+                    self.sources = sources
+                    self.sinks = sinks
 
                 def compile(self):
                     pass
 
                 def launch(self, symbol_table, wait_for):
-                    bottom = symbol_table[sources[0]]
-                    weights = symbol_table[sources[1]]
-                    bias = symbol_table[sources[2]]
-                    top = symbol_table[sinks[0]].T.view(hmarray)
+                    bottom = symbol_table[sources[0].name]
+                    weights = symbol_table[sources[1].name]
+                    bias = symbol_table[sources[2].name]
+                    top = symbol_table[sinks[0].name]
                     evt = sgemm(False, True, 1.0, bottom, 0, K, weights,
                                 0, K, 0.0, top, 0, N, M, N, K, wait_for=wait_for)
                     evt = sgemm(False, False, 1.0, bias_multiplier, 0,
