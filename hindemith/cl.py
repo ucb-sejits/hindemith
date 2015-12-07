aa58db044e28d7c2b005be857549cf4c7cb92557
Leonard Truong <leonardtruong@gmail.com>
2015-12-06 16:47:59 -0800

    Sort param names alphabetically

--- hindemith/cl.py
+++ hindemith/cl.py
@@ -97,7 +97,7 @@ if backend in {"ocl", "opencl", "OCL"}:
                         if param.name not in seen_params:
                             seen_params.add(param.name)
                             filtered.add(param)
-                self.params = list(filtered)
+                self.params = list(filtered).sort(key=lambda x: x.name)
                 params = []
                 sinks = set(sink.name for sink in self.sinks)
                 for param in self.params:
